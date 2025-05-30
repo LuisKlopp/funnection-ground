59fcbbcc6f4e06dbc3b3b760c2752eb25983a277
LuisKlopp <fbgus333@gmail.com>
2025-05-30 14:01:40 +0900

    Feat: 모달 변경

--- components/true-true-false/true-true-false-modal.tsx
+++ components/true-true-false/true-true-false-modal.tsx
@@ -41,7 +41,7 @@ export const TrueTrueFalseModal = ({
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
       <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="box-shadow-04 relative mx-4 flex h-[600px] w-[500px] flex-col items-center justify-evenly rounded-lg bg-[#d6ecf8] shadow-lg">
+      <div className="box-shadow-04 relative mx-4 flex w-[700px] flex-col items-center justify-evenly rounded-lg bg-[#d6ecf8] py-8 shadow-lg">
         <button
           className="absolute top-2 right-2 text-2xl text-gray-500 hover:text-gray-800"
           onClick={closeModal}
@@ -50,7 +50,7 @@ export const TrueTrueFalseModal = ({
         </button>
         <h2 className="text-xl font-semibold text-slate-700">
           <span className="text-[#4599a5]">{userId}</span>
-          &apos;님의 진진가
+          &apos;님의 진진진가
         </h2>
         <div className="fade-in-up mt-7 flex w-full px-20">
           <ul className="flex w-full flex-col gap-2">
@@ -75,7 +75,7 @@ export const TrueTrueFalseModal = ({
 
         <button
           onClick={() => setShowFake(true)}
-          className="rounded-lg bg-gray-700 px-6 py-4 text-[20px] text-white shadow-md hover:bg-gray-800"
+          className="mt-8 rounded-lg bg-gray-700 px-6 py-4 text-[20px] text-white shadow-md hover:bg-gray-800"
         >
           확인
         </button>
